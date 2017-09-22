372b90eee78f8020f82aa33e9e44a86ecb00a504
Admin <Admin@192.168.1.5>
2017-09-22 15:03:00 +1200

    Created basic JUnit test class

---
+++ WebApp/src/test/java/testPackage/TestClass.java
@@ -0,0 +1,10 @@
+package testPackage;
+
+import static org.mockito.Mockito.*;
+
+import dbAccess.CarInfo;
+import junit.framework.TestCase;
+
+public class TestClass extends TestCase {
+	CarInfo testCar;
+}
